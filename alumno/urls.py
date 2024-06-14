8bcda483e17243621c13d9b27af66e31569db920
nmunozm <nmunozm@utem.cl>
2024-06-14 20:28:58 +0000

    fix varios

--- alumno/urls.py
+++ alumno/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('crud',views.crud,name='crud'),
+    path('generos_list',views.genero_list,name='genero_list'),
+    path('generos_add',views.genero_add,name='genero_add'),
+    path('generos_edit',views.genero_edit,name='genero_edit'),
+    path('generos_del',views.genero_del,name='genero_del'),
+#print(path),
 ]
--- alumno/views.py
+++ alumno/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def crud(request):
-    return render(request,'alumnos/crud.html')
+    return render(request,'alumno/crud.html')
+
+def genero_list(request):
+    return render(request,'alumno/genero_list.html')
+
+def genero_add(request):
+    return render(request,'alumno/genero_add.html')
+
+def genero_edit(request):
+    return render(request,'alumno/genero_edit.html')
+
+def genero_del(request):
+    return render(request,'alumno/genero_del.html')
