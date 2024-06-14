141371ca09fab41fd172de74ab73d90e8fdcc343
nmunozm <nmunozm@utem.cl>
2024-06-14 20:01:21 +0000

    create migrations

--- instituto/urls.py
+++ instituto/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alumno/',include('alumnos.urls')),
+    path('alumno/',include('alumno.urls')),
 ]
